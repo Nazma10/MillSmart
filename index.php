41fd664387feea13a14db38e63a797d43d6d0ea5
M.N.F.Nazma <91309758+Nazma10@users.noreply.github.com>
2023-05-13 14:56:56 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+    include './config/database.php';
+    include './config/config.php';
+    include __DIR__ . './vendor/autoload.php';
+    // echo 'sammat';  
+    include './app/core.php';
+?>
